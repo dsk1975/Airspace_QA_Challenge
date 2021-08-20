d8548fbeec5e1d1f6d209357b41451172d6c5692
dsk1975 <dsko1975@gmail.com>
2021-08-19 18:55:21 -0700

    Delete old files

--- cypress/integration/UI/first_test.spec.js
+++
@@ -1,8 +0,0 @@
-describe('Heading text', () =>{
-    it('conteins a correct title', ()=>{
-        cy.visit('http://localhost:3000/example-1');
-        cy.get('h1')
-            .invoke('text')
-            .should('equal', 'My Awesome Wep Application');
-    });
-});
